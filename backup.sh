35b7e508ed1a7198bbc446e7f7e8261d00cd8e73
FC <i4never@163.com>
2017-04-01 11:08:16 +0800

    2017-04-01-11:08:16

--- backup.sh
+++ backup.sh
@@ -2,13 +2,13 @@
 
 echo -e "\033[31m===========================================\033[0m" 
 echo "This script is for backup hexo blog"
-echo -e "\033[31m===========================================\033[0m"
 echo "The following file will be uploaded:"
 echo -e "	\033[32m./themes\033[0m"
 echo -e "	\033[32m_config.yml\033[0m"
 echo -e "	\033[32m./source\033[0m"
 echo -e "	\033[32mpackage.json\033[0m"
 echo -e "	\033[32mbackup.sh\033[0m"
+echo -e "\033[31m===========================================\033[0m"
 
 echo -e "Change branch to \"hexo\""
 cd i4never.github.io
