c46ee54eff6931b4ca702c021115faa55307082c
FC <i4never@163.com>
2017-04-01 10:56:35 +0800

    2017-04-01-10:56:35

--- backup.sh
+++ backup.sh
@@ -12,14 +12,14 @@ echo -e "	\033[32mbackup.sh\033[0m"
 echo -e "Change branch to \"hexo\""
 cd i4never.github.io
 # git checkout hexo
-cd ..
 
-cp -r ./themes ./i4never.github.io/themes
-cp _config.yml ./i4never.github.io/_config.yml
-cp package.json ./i4never.github.io/package.json
-cp backup.sh ./i4never.github.io/backup.sh
+cp -r ../themes ./themes
+cp ../_config.yml ./_config.yml
+cp ../package.json ./package.json
+cp ../backup.sh ./backup.sh
 
 d="`date +%Y-%m-%d-%H:%M:%S`"
 
 git add .
 git commit -m $d
+git push
